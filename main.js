87fcc9fa8a1676cc1335aba0c71eceacc2f65fa3
Robert Cutright <rdcpcguy@gmail.com>
2018-04-11 01:49:26 -0500

    Working.

--- main.js
+++ main.js
@@ -6,40 +6,42 @@ const url = require('url');
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({
-    width: 353,
-    height: 998,
-    resizable: false,
-    frame: false,
-    autoHideMenuBar: true,
-    darkTheme: true,
-    movable: true,
-    transparent: true
-  });
+    win = new BrowserWindow({
+        width: 353,
+        height: 998,
+        resizable: false,
+        frame: false,
+        autoHideMenuBar: true,
+        darkTheme: true,
+        movable: true,
+        hasShadow: true,
+        transparent: true,
+        maximizable: false
+    });
 
-  win.loadURL(
-    url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    })
-  );
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, 'index.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    );
 
-  win.on('closed', () => {
-    win = null;
-  });
+    win.on('closed', () => {
+        win = null;
+    });
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow();
-  }
+    if (win === null) {
+        createWindow();
+    }
 });
--- renderer.js
+++ renderer.js
@@ -1,119 +1,157 @@
-var pathFinders = document.getElementById('path-finders');
-var textInputs = document.getElementById('text-inputs');
-
-function loadData(attr) {
-  return localStorage.getItem(attr);
-}
-
-function saveData(attr, value) {
-  localStorage.setItem(attr, value);
-}
-
-function handleValue(element = event.target, newLabel, cover) {
-  // let element = event.target;
-  console.log(element);
-  if (element.files) {
-    cover.setAttribute(
-      'style',
-      'color: transparent; background-color: transparent'
-    );
-    saveData(newLabel, 'path', element.files[0].path);
-    saveData(newLabel, 'name', element.files[0].name);
-    return;
-  }
-  if (element.value) {
-    element.setAttribute('value', element.value);
-    saveData(newLabel, 'value', element.value);
-    return;
-  }
-}
-
 function newPathFinder(newLabel, elementToAppendTo) {
-  let previous = loadData(newLabel);
+    let previous = loadData(newLabel);
 
-  let div = document.createElement('div');
-  let input = document.createElement('INPUT');
-  let label = document.createElement('h2');
-  let existLabel = document.createElement('p');
+    let div = document.createElement('div');
+    let input = document.createElement('INPUT');
+    let label = document.createElement('h2');
 
-  div.setAttribute('class', 'container');
-  input.setAttribute('type', 'file');
-  input.setAttribute('class', newLabel);
-  label.setAttribute('htmlFor', newLabel);
+    div.setAttribute('class', 'container');
+    input.setAttribute('type', 'file');
+    input.setAttribute('class', newLabel);
+    label.setAttribute('htmlFor', newLabel);
 
-  elementToAppendTo.appendChild(div);
-  div.appendChild(label);
-  div.appendChild(input);
-  div.appendChild(existLabel);
+    elementToAppendTo.appendChild(div);
+    div.appendChild(label);
+    div.appendChild(input);
 
-  existLabel.setAttribute('class', 'existing');
-  input.setAttribute('path', previous || '');
-  input.addEventListener('change', e => handleValue(e, newLabel, existLabel));
-  existLabel.innerHTML = previous;
-  label.innerHTML = newLabel;
-  return input;
+    let path = previous.split(`\\`);
+    let existLabel = document.createElement('p');
+    div.appendChild(existLabel);
+    existLabel.setAttribute('class', 'existing');
+    existLabel.innerHTML = path[path.length - 1];
+
+    input.setAttribute('path', previous || '');
+    input.setAttribute('content', previous || '');
+    input.addEventListener('change', e => handleValue(e, newLabel, div));
+    label.innerHTML = newLabel;
+    return input;
 }
 
 function newInput(newLabel, elementToAppendTo, type) {
-  var previous = loadData(newLabel);
-  var input = document.createElement('INPUT');
-  var label = document.createElement('h2');
-  type = type || 'text';
-  input.setAttribute('id', type);
-  input.setAttribute('type', type);
-  input.setAttribute('class', newLabel);
-  label.setAttribute('htmlFor', newLabel);
-  if (previous) {
-    console.log('Loaded Data:', newLabel, previous);
-    input.setAttribute('value', previous.value);
-    input.setAttribute('content', previous.value);
-  }
-  elementToAppendTo.appendChild(label);
-  elementToAppendTo.appendChild(input);
-  input.addEventListener('change', e => handleValue(e, newLabel));
-  label.innerHTML = newLabel;
-  return input;
+    var previous = loadData(newLabel);
+
+    var input = document.createElement('INPUT');
+    var label = document.createElement('h2');
+    type = type || 'text';
+    input.setAttribute('id', type);
+    input.setAttribute('type', type);
+    input.setAttribute('class', newLabel);
+    label.setAttribute('htmlFor', newLabel);
+    if (previous) {
+        input.setAttribute('value', previous);
+        input.setAttribute('content', previous);
+    }
+    elementToAppendTo.appendChild(label);
+    elementToAppendTo.appendChild(input);
+    input.addEventListener('keydown', e => handleValue(e, newLabel));
+    label.innerHTML = newLabel;
+    return input;
 }
 
 function newButton(newLabel, elementToAppendTo) {
-  var button = document.createElement('button');
-  elementToAppendTo.appendChild(button);
-  button.setAttribute('name', newLabel);
-  button.setAttribute('text', newLabel);
-  button.setAttribute('value', newLabel);
-  button.innerHTML = newLabel;
-  return button;
+    var button = document.createElement('button');
+    elementToAppendTo.appendChild(button);
+    button.setAttribute('name', newLabel);
+    button.setAttribute('text', newLabel);
+    button.setAttribute('value', newLabel);
+    button.innerHTML = newLabel;
+    return button;
+}
+function loadData(attr) {
+    return localStorage.getItem(attr);
 }
 
-var signer = newPathFinder('Signer', pathFinders);
-var modl = newPathFinder('Modl', pathFinders);
-var keyStore = newPathFinder('Key-Store', pathFinders);
-var certificate = newPathFinder('Certificate', pathFinders);
+function saveData(attr, value) {
+    localStorage.setItem(attr, value);
+}
 
-var alias = newInput('Alias', textInputs, 'text');
-var aliasPassword = newInput('Alias Password', textInputs, 'password');
-var keyStorePassword = newInput('Key Store Password', textInputs, 'password');
-var compileButton = newButton('Compile', textInputs);
-
-function handleCompile() {
-  let path = `java -jar ${signer.getAttribute('path')} -keystore=${
-    keyStore.files[0].path
-  } -keystore-pwd=${keyStorePassword.value} -alias=${alias.value} -alias-pwd=${
-    aliasPassword.value
-  } -chain=${certificate.files[0].path} -module-in=${
-    modl.files[0].path
-  } -module-out=${modl.files[0].path}`;
-
-  const exec = require('child_process').exec;
-  exec(path, (error, stdout, stderr) => {
-    if (error instanceof Error) {
-      console.error('stderr', stderr);
-      throw error;
+function handleValue(element, newLabel, div) {
+    element = element.target;
+    console.log(element, newLabel, div);
+    if (div) {
+        saveData(newLabel, element.files[0].path);
+        let path = loadData(newLabel);
+        path = path.split(`\\`);
+        let existLabel = document.createElement('p');
+        div.appendChild(existLabel);
+        existLabel.setAttribute('class', 'existing');
+        existLabel.innerHTML = path[path.length - 1];
+        return;
+    } else {
+        element.setAttribute('value', element.value);
+        saveData(newLabel, element.value);
+        return;
     }
-    var success = document.createElement('p');
-    textInputs.appendChild(success);
-    success.innerHTML = 'Successfully Signed: ' + modl.files[0].path;
-  });
 }
 
-compileButton.addEventListener('click', handleCompile);
+function execute(config, element) {
+    const path = `
+  java -jar ${config.signer}
+  -keystore=${config.keystore}
+  -keystore-pwd=${config.keystorePassword}
+  -alias=${config.alias}
+  -alias-pwd=${config.aliasPassword}
+  -chain=${config.certificate}
+  -module-in=${config.path}
+  -module-out=${config.path}
+  `;
+    const exec = require('child_process').exec;
+    exec(path, (error, stdout, stderr) => {
+        if (error instanceof Error) {
+            console.error('stderr', stderr);
+            throw error;
+        }
+        var success = document.createElement('p');
+        element.appendChild(success);
+        success.innerHTML = 'Successfully Signed';
+    });
+}
+
+function exit() {
+    window.close();
+}
+
+function getPath(element) {
+    return element.getAttribute('path');
+}
+
+function getValue(element) {
+    return element.getAttribute('value');
+}
+
+function main() {
+    var exitButton = document.getElementById('exit');
+    var textInputs = document.getElementById('text-inputs');
+    var pathFinders = document.getElementById('path-finders');
+    var alias = newInput('Alias', textInputs, 'text');
+    var modl = newPathFinder('Modl', pathFinders);
+    var signer = newPathFinder('Signer', pathFinders);
+    var keyStore = newPathFinder('Key-Store', pathFinders);
+    var certificate = newPathFinder('Certificate', pathFinders);
+    var aliasPassword = newInput('Alias Password', textInputs, 'password');
+    var keyStorePassword = newInput(
+        'Key Store Password',
+        textInputs,
+        'password'
+    );
+    var compileButton = newButton('Compile', textInputs);
+
+    exitButton.addEventListener('click', exit);
+    compileButton.addEventListener('click', () =>
+        execute(
+            {
+                signer: getPath(signer),
+                keystore: getPath(keyStore),
+                keystorePassword: getValue(keyStorePassword),
+                aliasPassword: getValue(aliasPassword),
+                alias: getValue(alias),
+                path: getPath(modl),
+                modl: getPath(modl),
+                certificate: getPath(certificate)
+            },
+            textInputs
+        )
+    );
+}
+
+main();
